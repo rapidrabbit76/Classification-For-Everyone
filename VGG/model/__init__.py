37328ba231fd8b78f165e4925acb45d3fa8e6b59
YSLEE <yslee.dev@gmail.com>
2022-01-05 11:09:54 +0000

    refactor: vgg model build refactoring
    build vgg architectures using model_type

--- VGG/model/__init__.py
+++ VGG/model/__init__.py
@@ -1,1 +1,1 @@
-from .models import VGG11, VGG13, VGG16_C, VGG16_D, VGG19
+from .models import VGGModel
--- VGG/model/models.py
+++ VGG/model/models.py
@@ -1,193 +1,89 @@
 from math import log
+from xml.etree.ElementInclude import include
 import torch
 from torch import nn
 
 from .blocks import Classifier, ConvBlock
 
+VGG_types = {
+    "VGG11": [64, "M", 128, "M", 256, 256, "M", 512, 512, "M", 512, 512, "M"],
+    "VGG13": [64, 64, "M", 128, 128, "M", 256, 256, "M", 512, 512, "M", 512, 512, "M"],
+    "VGG16": [
+        64,
+        64,
+        "M",
+        128,
+        128,
+        "M",
+        256,
+        256,
+        256,
+        "M",
+        512,
+        512,
+        512,
+        "M",
+        512,
+        512,
+        512,
+        "M",
+    ],
+    "VGG19": [
+        64,
+        64,
+        "M",
+        128,
+        128,
+        "M",
+        256,
+        256,
+        256,
+        256,
+        "M",
+        512,
+        512,
+        512,
+        512,
+        "M",
+        512,
+        512,
+        512,
+        512,
+        "M",
+    ],
+}
 
-class VGG11(nn.Module):
+
+class VGGModel(nn.Module):
     def __init__(
         self,
+        model_type: str = "VGG11",
         image_channals: int = 3,
         n_classes: int = 1000,
         use_bn: bool = False,
-        image_size: int = 224,
     ) -> None:
         super().__init__()
 
-        self.feature_extractor = nn.Sequential(
-            # 1 conv
-            ConvBlock(image_channals, 64, norm=use_bn),
-            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
-            # 2 conv
-            ConvBlock(64, 128, norm=use_bn),
-            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
-            # 3 conv
-            ConvBlock(128, 256, norm=use_bn),
-            ConvBlock(256, 256, norm=use_bn),
-            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
-            # 4 conv
-            ConvBlock(256, 512, norm=use_bn),
-            ConvBlock(512, 512, norm=use_bn),
-            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
-            # 5 conv
-            ConvBlock(512, 512, norm=use_bn),
-            ConvBlock(512, 512, norm=use_bn),
-            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
-        )
+        layers = []
+
+        assert (
+            model_type in VGG_types.keys()
+        ), f"{model_type} is not in {' '.join(VGG_types.keys())}"
+
+        in_channels = image_channals
+        for x in VGG_types[model_type]:
+            if type(x) == int:
+                layers.append(ConvBlock(in_channels, x, norm=use_bn))
+                in_channels = x
+            elif x == "M":
+                layers.append(nn.MaxPool2d(2, 2))
+
+        self.feature_extractor = nn.Sequential(*layers)
         self.classfier = nn.Sequential(
             nn.Flatten(),
-            Classifier((512 * (image_size // 2 ** 5) ** 2), 4096, n_classes, 0.5),
+            Classifier(512 * 7 * 7, 4096, n_classes, 0.5),
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.feature_extractor(x)
         return self.classfier(x)
-
-
-class VGG13(VGG11):
-    def __init__(
-        self,
-        image_channals: int = 3,
-        n_classes: int = 1000,
-        use_bn: bool = False,
-        image_size: int = 224,
-    ) -> None:
-        super().__init__(n_classes, use_bn, image_size)
-
-        self.feature_extractor = nn.Sequential(
-            # 1 conv
-            ConvBlock(image_channals, 64, norm=use_bn),
-            ConvBlock(64, 64, norm=use_bn),
-            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
-            # 2 conv
-            ConvBlock(64, 128, norm=use_bn),
-            ConvBlock(128, 128, norm=use_bn),
-            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
-            # 3 conv
-            ConvBlock(128, 256, norm=use_bn),
-            ConvBlock(256, 256, norm=use_bn),
-            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
-            # 4 conv
-            ConvBlock(256, 512, norm=use_bn),
-            ConvBlock(512, 512, norm=use_bn),
-            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
-            # 5 conv
-            ConvBlock(512, 512, norm=use_bn),
-            ConvBlock(512, 512, norm=use_bn),
-            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
-        )
-
-
-class VGG16_C(VGG11):
-    def __init__(
-        self,
-        image_channals: int = 3,
-        n_classes: int = 1000,
-        use_bn: bool = False,
-        image_size: int = 224,
-    ) -> None:
-        super().__init__(n_classes, use_bn, image_size)
-
-        self.feature_extractor = nn.Sequential(
-            # 1 conv
-            ConvBlock(image_channals, 64, norm=use_bn),
-            ConvBlock(64, 64, norm=use_bn),
-            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
-            # 2 conv
-            ConvBlock(64, 128, norm=use_bn),
-            ConvBlock(128, 128, norm=use_bn),
-            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
-            # 3 conv
-            ConvBlock(128, 256, norm=use_bn),
-            ConvBlock(256, 256, norm=use_bn),
-            ConvBlock(256, 256, kernel_size=1, padding=0, norm=use_bn),
-            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
-            # 4 conv
-            ConvBlock(256, 512, norm=use_bn),
-            ConvBlock(512, 512, norm=use_bn),
-            ConvBlock(512, 512, kernel_size=1, padding=0, norm=use_bn),
-            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
-            # 5 conv
-            ConvBlock(512, 512, norm=use_bn),
-            ConvBlock(512, 512, norm=use_bn),
-            ConvBlock(512, 512, kernel_size=1, padding=0, norm=use_bn),
-            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
-        )
-
-
-class VGG16_D(VGG11):
-    def __init__(
-        self,
-        image_channals: int = 3,
-        n_classes: int = 1000,
-        use_bn: bool = False,
-        image_size: int = 224,
-    ) -> None:
-        super().__init__(n_classes, use_bn, image_size)
-
-        self.feature_extractor = nn.Sequential(
-            # 1 conv
-            ConvBlock(image_channals, 64, norm=use_bn),
-            ConvBlock(64, 64, norm=use_bn),
-            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
-            # 2 conv
-            ConvBlock(64, 128, norm=use_bn),
-            ConvBlock(128, 128, norm=use_bn),
-            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
-            # 3 conv
-            ConvBlock(128, 256, norm=use_bn),
-            ConvBlock(256, 256, norm=use_bn),
-            ConvBlock(256, 256, norm=use_bn),
-            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
-            # 4 conv
-            ConvBlock(256, 512, norm=use_bn),
-            ConvBlock(512, 512, norm=use_bn),
-            ConvBlock(512, 512, norm=use_bn),
-            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
-            # 5 conv
-            ConvBlock(512, 512, norm=use_bn),
-            ConvBlock(512, 512, norm=use_bn),
-            ConvBlock(512, 512, norm=use_bn),
-            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
-        )
-
-
-class VGG19(VGG11):
-    def __init__(
-        self,
-        image_channals: int = 3,
-        n_classes: int = 1000,
-        use_bn: bool = False,
-        image_size: int = 224,
-    ) -> None:
-        super().__init__(n_classes, use_bn, image_size)
-
-        self.feature_extractor = nn.Sequential(
-            # 1 conv
-            ConvBlock(image_channals, 64, norm=use_bn),
-            ConvBlock(64, 64, norm=use_bn),
-            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
-            # 2 conv
-            ConvBlock(64, 128, norm=use_bn),
-            ConvBlock(128, 128, norm=use_bn),
-            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
-            # 3 conv
-            ConvBlock(128, 256, norm=use_bn),
-            ConvBlock(256, 256, norm=use_bn),
-            ConvBlock(256, 256, norm=use_bn),
-            ConvBlock(256, 256, norm=use_bn),
-            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
-            # 4 conv
-            ConvBlock(256, 512, norm=use_bn),
-            ConvBlock(512, 512, norm=use_bn),
-            ConvBlock(512, 512, norm=use_bn),
-            ConvBlock(512, 512, norm=use_bn),
-            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
-            # 5 conv
-            ConvBlock(512, 512, norm=use_bn),
-            ConvBlock(512, 512, norm=use_bn),
-            ConvBlock(512, 512, norm=use_bn),
-            ConvBlock(512, 512, norm=use_bn),
-            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
-        )
