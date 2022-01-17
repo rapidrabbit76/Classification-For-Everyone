e33e63d67139091cd9a008c90cd6b8a39d35d212
zkdlfrlwl2 <zkdlfrlwl3@naver.com>
2022-01-17 20:40:13 +0900

    feat: create GoogLeNet model

---
+++ GoogLeNet/model/block.py
@@ -0,0 +1,112 @@
+import torch
+import torch.nn as nn
+
+
+class ConvBlock(nn.module):
+
+    def __init__(
+            self,
+            in_channels: int,
+            out_channels: int,
+            kernel_size: int,
+            stride: int = 1,
+            padding: str = 'same',
+            bias: bool = True,
+    ):
+        super().__init__()
+        self.conv = nn.Conv2d(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
+            bias=bias,
+        )
+        self.bn = nn.BatchNorm2d(num_features=out_channels, eps=1e-3)
+        self.act = nn.ReLU()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.conv(x)
+        x = self.bn(x)
+        return self.act(x)
+
+
+class InceptionBlock(ConvBlock):
+
+    def __init__(
+            self,
+            in_channels: int,
+            out_channels_1by1: int,
+            out_channels_3by3_reduce: int,
+            out_channels_3by3: int,
+            out_channels_5by5_reduce: int,
+            out_channels_5by5: int,
+            out_channels_pool_proj: int,
+    ):
+        super().__init__()
+        self.in_channels = in_channels
+
+        self.out_channels_1by1 = out_channels_1by1
+        self.out_channels_3by3_reduce = out_channels_3by3_reduce
+        self.out_channels_3by3 = out_channels_3by3
+        self.out_channels_5by5_reduce = out_channels_5by5_reduce
+        self.out_channels_5by5 = out_channels_5by5
+        self.out_channels_pool_proj = out_channels_pool_proj
+
+        self.maxpooling2d = nn.MaxPool2d(kernel_size=3, stride=1, padding='same')
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x1 = ConvBlock(self.in_channels, self.out_channels_1by1, kernel_size=1)(x)
+
+        x2_1 = ConvBlock(self.in_channels, self.out_channels_3by3_reduce, kernel_size=1)(x)
+        x2_2 = ConvBlock(self.out_channels_3by3_reduce, self.out_channels_3by3, kernel_size=3)(x2_1)
+
+        x3_1 = ConvBlock(self.in_channels, self.out_channels_5by5_reduce, kernel_size=1)(x)
+        x3_2 = ConvBlock(self.out_channels_5by5_reduce, self.out_channels_5by5, kernel_size=5)(x3_1)
+
+        x4_1 = self.maxpooling2d(x)
+        x4_2 = ConvBlock(self.in_channels, self.out_channels_pool_proj, kernel_size=1)(x4_1)
+
+        return torch.cat([x1, x2_2, x3_2, x4_2], dim=1)
+
+
+class AuxClassifier(nn.Module):
+
+    def __init__(
+            self,
+            in_features: int,
+            dim: int,
+            out_features: int,
+            dropout_rate: float = 0.4,
+    ):
+        super().__init__()
+
+        self.classifier = nn.Sequential(
+            nn.Dropout(dropout_rate),
+            nn.Linear(in_features, dim),
+            nn.ReLU(),
+            nn.Dropout(dropout_rate),
+            nn.Linear(dim, out_features)
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.classifier(x)
+
+
+class Classifier(nn.Module):
+
+    def __init__(
+            self,
+            in_features: int,
+            out_features: int,
+            dropout_rate: float = 0.4,
+    ):
+        super().__init__()
+
+        self.classifier = nn.Sequential(
+            nn.Dropout(dropout_rate),
+            nn.Linear(in_features, out_features),
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.classifier(x)
---
+++ GoogLeNet/train.py
@@ -0,0 +1,154 @@
+import os
+import sys
+import pytorch_lightning as pl
+from pytorch_lightning.utilities.seed import seed_everything
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim.lr_scheduler import MultiStepLR
+from pytorch_lightning.callbacks import (
+    EarlyStopping,
+    TQDMProgressBar,
+)
+from pytorch_lightning.loggers import WandbLogger
+from torchmetrics import Accuracy
+
+import datamodule
+from model import GoogLeNet
+
+sys.path.insert(1, os.path.abspath('..'))
+import utils
+
+
+class GoogLeNetModel(pl.LightningModule):
+
+    def __init__(
+            self,
+            image_channels: int,
+            n_classes: int,
+            lr: float,
+    ):
+        super().__init__()
+        self.save_hyperparameters()
+
+        self.model = GoogLeNet(
+            self.hparams.image_channels,
+            self.hparams.n_classes,
+        )
+
+        self.loss = nn.CrossEntropyLoss()
+        self.accuracy = Accuracy()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.model(x)
+
+    def step(self, batch):
+        x, y = batch
+        logit = self(x)
+        loss = self.loss(logit, y)
+        preds = torch.argmax(logit, dim=1)
+        acc = self.accuracy(preds, y)
+        return loss, acc
+
+    def training_step(self, batch, batch_idx) -> torch.Tensor:
+        loss, acc = self.step(batch)
+        self.log('train_loss', loss, prog_bar=True)
+        self.log('train_acc', acc, prog_bar=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx) -> torch.Tensor:
+        loss, acc = self.step(batch)
+        self.log('val_loss', loss, prog_bar=True)
+        self.log('val_acc', acc, prog_bar=True)
+        return loss
+
+    def test_step(self, batch, batch_idx) -> torch.Tensor:
+        loss, acc = self.step(batch)
+        self.log('test_loss', loss, prog_bar=True)
+        self.lof('test_acc', acc, prog_bar=True)
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = optim.SGD(
+            self.parameters(),
+            lr=self.hparams.lr,
+            momentum=0.9,
+        )
+        scheduler_dict = {
+            'scheduler': MultiStepLR(
+                optimizer,
+                milestones=utils.get_milestones(
+                    epoch=self.hparams.epochs,
+                    step=self.hparams.lr_step
+                ),
+                gamma=0.92,
+                verbose=True,
+            )
+        }
+        return {'optimizer': optimizer, 'lr_scheduler': scheduler_dict}
+
+
+def train():
+    # Hyperparameters
+    config = utils.get_config()
+    hparams = config.hparams
+    seed_everything(config.seed)
+
+    # Dataloader
+    dm = datamodule.CIFAR10DataModule(
+        config.data_dir,
+        image_size=config.image_size,
+        batch_size=hparams.batch_size,
+    )
+
+    # Model
+    googlenet = GoogLeNetModel(
+        image_channels=config.image_channels,
+        n_classes=config.n_classes,
+        lr=hparams.lr
+    )
+
+    # Logger
+    wandb_logger = WandbLogger(
+        name=f'{config.project_name}-{config.dataset}',
+        project=config.project_name,
+        save_dir=config.save_dir,
+        log_model='all',
+    )
+    wandb_logger.experiment.config.update(hparams)
+    wandb_logger.watch(googlenet, log='all', log_freq=100)
+
+    # Trainer setting
+    callbacks = [
+        EarlyStopping(
+            monitor='val_acc',
+            min_delta=0.00,
+            patience=5,
+            verbose=True,
+            mode='max',
+        ),
+        TQDMProgressBar(refresh_rate=10),
+    ]
+
+    trainer: pl.Trainer = pl.Trainer(
+        logger=wandb_logger,
+        gpus=1,
+        max_epochs=hparams.epochs,
+        callbacks=callbacks,
+    )
+
+    # Train
+    trainer.fit(googlenet, datamodule=dm)
+    trainer.test(googlenet, datamodule=dm)
+
+    # Finish
+    wandb_logger.experiment.unwatch(googlenet)
+
+    # Model to Torchscript
+    script = googlenet.to_torchscript()
+    torch.jit.save(script, config.torchscript_model_save_path)
+    wandb_logger.experiment.save(config.torchscript_model_save_path)
+
+
+if __name__=='__main':
+    train()
