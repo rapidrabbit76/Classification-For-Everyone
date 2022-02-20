8c3f2fd0627443bbcbf7e22b1090a403475767a2
yslee <yslee.dev@gmail.com>
2022-02-20 22:56:06 +0900

    refactor: rm vgg model init weights

--- models/VGG/models.py
+++ models/VGG/models.py
@@ -1,5 +1,4 @@
-from math import log
-from xml.etree.ElementInclude import include
+from typing import Final, Dict
 import torch
 from torch import nn
 
@@ -7,7 +6,7 @@ from .blocks import Classifier, ConvBlock
 
 __all__ = ["VGGModel", "VGG11", "VGG13", "VGG16", "VGG19"]
 
-MODEL_TYPES = {
+MODEL_TYPES: Final[Dict] = {
     # fmt: off
     "VGG11": [64,  "M", 128, "M", 256, 256, "M", 512, 512, "M", 
               512, 512, "M"],
@@ -58,17 +57,3 @@ class VGGModel(nn.Module):
         x = self.avgpool(x)
         return self.classfier(x)
 
-    def initialize_weights(self):
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(
-                    m.weight,
-                    mode="fan_out",
-                    nonlinearity="relu",
-                )
-            elif isinstance(m, nn.BatchNorm2d):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
-            elif isinstance(m, nn.Linear):
-                nn.init.normal_(m.weight, 0, 0.01)
-                nn.init.constant_(m.bias, 0)
