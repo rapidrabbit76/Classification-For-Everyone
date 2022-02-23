77fef4e154397e7498020e5c004fddbba6f36453
yslee <yslee.dev@gmail.com>
2022-02-23 21:00:24 +0900

    refactor: squeezenet

--- models/Squeezenet/__init__.py
+++ models/Squeezenet/__init__.py
@@ -1,1 +1,2 @@
-from .models import SqueezeNet
+from models.Squeezenet.lightning_model import LitSqueezeNet
+from models.Squeezenet.models import SqueezeNet
--- models/Squeezenet/blocks.py
+++ models/Squeezenet/blocks.py
@@ -1,28 +1,21 @@
 import torch
 from torch import nn
 
+Activation = nn.ReLU
+
 
 class ConvBlock(nn.Module):
-
-    def __init__(
-        self,
-        in_channels: int,
-        out_channels: int,
-        kernel_size: int = 3,
-        stride: int = 1,
-        padding: int = 0,
-        bias: bool = True,
-    ):
+    def __init__(self, inp: int, outp: int, k: int = 3, s: int = 1, p: int = 0):
         super().__init__()
         self.conv = nn.Conv2d(
-            in_channels,
-            out_channels,
-            kernel_size=kernel_size,
-            stride=stride,
-            padding=padding,
-            bias=bias,
+            inp,
+            outp,
+            kernel_size=k,
+            stride=s,
+            padding=p,
+            bias=False,
         )
-        self.act = nn.ReLU(inplace=True)
+        self.act = Activation(inplace=True)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.conv(x)
@@ -30,23 +23,17 @@ class ConvBlock(nn.Module):
 
 
 class FireModule(nn.Module):
-
     def __init__(
         self,
-        in_channels: int,
-        out_channels: int,
-        e1x1_channels: int,
-        e3x3_channels: int,
+        inp: int,
+        outp: int,
+        e1x1: int,
+        e3x3: int,
     ):
         super().__init__()
-        self.squeeze = ConvBlock(in_channels, out_channels, 1)
-        self.e1x1 = ConvBlock(out_channels, e1x1_channels, 1)
-        self.e3x3 = ConvBlock(
-            out_channels,
-            e3x3_channels,
-            3,
-            padding=1,
-        )
+        self.squeeze = ConvBlock(inp, outp, 1)
+        self.e1x1 = ConvBlock(outp, e1x1, 1)
+        self.e3x3 = ConvBlock(outp, e3x3, 3, p=1)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.squeeze(x)
