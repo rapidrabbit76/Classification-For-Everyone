cec65fac284ce79e22924eb8ad770ccfceaeb75c
yslee <yslee.dev@gmail.com>
2022-02-28 21:47:27 +0900

    refactor: mnist datamodule

--- datamodules/MNIST.py
+++ datamodules/MNIST.py
@@ -1,20 +1,21 @@
 from typing import *
 import pytorch_lightning as pl
-from torch.utils.data import DataLoader, Subset
-from torchvision.datasets import MNIST, FashionMNIST
+from torch.utils.data import DataLoader, Subset, Dataset
+from torchvision.datasets import MNIST
 from sklearn.model_selection import train_test_split
 import numpy as np
 
 
-class MnistDataModule(pl.LightningDataModule):
+class MnistDataModuleBase(pl.LightningDataModule):
     def __init__(
         self,
+        DATASET: Dataset,
         root_dir: str,
         train_transforms: Callable,
         val_transforms: Callable,
         test_transforms: Callable,
-        batch_size: int = 1,
-        num_workers: int = 8,
+        batch_size: int,
+        num_workers: int,
     ):
         super().__init__()
         self.save_hyperparameters(
@@ -24,19 +25,20 @@ class MnistDataModule(pl.LightningDataModule):
                 "num_workers": num_workers,
             },
         )
+        self.Dataset = DATASET
         self.train_transforms = train_transforms
         self.val_transforms = val_transforms
         self.test_transforms = test_transforms
 
     def prepare_data(self) -> None:
         """Dataset download"""
-        MNIST(self.hparams.root_dir, train=True, download=True)
-        MNIST(self.hparams.root_dir, train=False, download=True)
+        self.Dataset(self.hparams.root_dir, train=True, download=True)
+        self.Dataset(self.hparams.root_dir, train=False, download=True)
 
     def setup(self, stage: Optional[str] = None) -> None:
         if stage == "fit" or stage is None:
             # split dataset to train, val
-            ds = MNIST(self.hparams.root_dir, train=True, download=False)
+            ds = self.Dataset(self.hparams.root_dir, train=True, download=False)
             targets = ds.targets
             train_idx, val_idx = train_test_split(
                 np.arange(len(targets)),
@@ -47,7 +49,7 @@ class MnistDataModule(pl.LightningDataModule):
 
             # build dataset, different transforms
             self.train_ds = Subset(
-                MNIST(
+                self.Dataset(
                     self.hparams.root_dir,
                     train=True,
                     transform=self.train_transforms,
@@ -56,7 +58,7 @@ class MnistDataModule(pl.LightningDataModule):
                 train_idx,
             )
             self.val_ds = Subset(
-                MNIST(
+                self.Dataset(
                     self.hparams.root_dir,
                     train=True,
                     transform=self.val_transforms,
@@ -95,49 +97,3 @@ class MnistDataModule(pl.LightningDataModule):
             shuffle=False,
             num_workers=self.hparams.num_workers,
         )
-
-
-class FashionMnistDataModule(MnistDataModule):
-    def prepare_data(self) -> None:
-        """Dataset download"""
-        FashionMNIST(self.hparams.root_dir, train=True, download=True)
-        FashionMNIST(self.hparams.root_dir, train=False, download=True)
-
-    def setup(self, stage: Optional[str] = None) -> None:
-        if stage == "fit" or stage is None:
-            # split dataset to train, val
-            ds = FashionMNIST(self.hparams.root_dir, train=True, download=False)
-            targets = ds.targets
-            train_idx, val_idx = train_test_split(
-                np.arange(len(targets)),
-                test_size=0.2,
-                shuffle=True,
-                stratify=targets,
-            )
-
-            # build dataset, different transforms
-            self.train_ds = Subset(
-                FashionMNIST(
-                    self.hparams.root_dir,
-                    train=True,
-                    transform=self.train_transforms,
-                    download=False,
-                ),
-                train_idx,
-            )
-            self.val_ds = Subset(
-                FashionMNIST(
-                    self.hparams.root_dir,
-                    train=True,
-                    transform=self.val_transforms,
-                    download=False,
-                ),
-                val_idx,
-            )
-
-        if stage == "test" or stage is None:
-            self.test_ds = FashionMNIST(
-                self.hparams.root_dir,
-                train=False,
-                transform=self.test_transforms,
-            )
