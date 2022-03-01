7203760ad42cf9cda65a69b9fb675cff31e05544
yslee <yslee.dev@gmail.com>
2022-03-01 12:57:32 +0900

    test: ShuffleNet torch model

---
+++ tests/ShuffleNet/test_model.py
@@ -0,0 +1,10 @@
+import pytest
+
+
+@pytest.mark.parametrize("mode", ["train", "test"])
+def test_model_inference(config, model, mode, batch):
+    model = model if mode == "train" else model.eval()
+    image, target = batch
+    x = model(image)
+    size = x.shape
+    assert size[1] == config.num_classes
