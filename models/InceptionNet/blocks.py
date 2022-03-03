8ffa749c47424e4d1381ef8c74038fb9b8abf02e
yslee <yslee.dev@gmail.com>
2022-03-04 00:18:50 +0900

    refactor: InceptionNet pytorch model

--- models/InceptionNet/blocks.py
+++ models/InceptionNet/blocks.py
@@ -1,76 +1,55 @@
-import torch
-from torch import nn
+from typing import *
 
-from typing import Tuple, Union, List
+import torch
+from torch import Tensor, nn
 
 _size_2_t = Union[int, Tuple[int, int]]
 
+Normalization = nn.BatchNorm2d
+Activation = nn.ReLU
+
 
 class ConvBlock(nn.Module):
-
     def __init__(
         self,
-        in_channels: int,
-        out_channels: int,
-        kernel_size: _size_2_t = 3,
-        stride: _size_2_t = 1,
-        padding: _size_2_t = 1,
-        norm: bool = False,
-        bias: bool = False,
+        inp: int,
+        outp: int,
+        k: _size_2_t = 3,
+        s: _size_2_t = 1,
+        p: _size_2_t = 1,
     ):
         super().__init__()
-        self.conv = nn.Conv2d(
-            in_channels,
-            out_channels,
-            kernel_size=kernel_size,
-            stride=stride,
-            padding=padding,
-            bias=bias,
-        )
-        self.norm = nn.BatchNorm2d(out_channels,
-                                   eps=0.001) if norm else nn.Identity()
-        self.act = nn.ReLU()
+        layer = [nn.Conv2d(inp, outp, k, s, p, bias=False)]
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = self.conv(x)
-        x = self.norm(x)
-        return self.act(x)
+        layer += [Normalization(outp, eps=0.001)]
+        layer += [Activation()]
+        self.block = nn.Sequential(*layer)
+
+    def forward(self, x: Tensor) -> Tensor:
+        return self.block(x)
 
 
 class Inceptionx3(nn.Module):
-
-    def __init__(
-        self,
-        in_channels: int,
-        dims: List[int],
-        norm: bool,
-    ) -> None:
+    def __init__(self, inp: int, dims: List[int]) -> None:
         super().__init__()
         self.branch1 = nn.Sequential(
-            ConvBlock(in_channels, dims[0], 1, stride=1, padding=0, norm=norm),
-            ConvBlock(dims[0], dims[0], 3, stride=1, padding=1, norm=norm),
-            ConvBlock(dims[0], dims[0], 3, stride=1, padding=1, norm=norm),
+            ConvBlock(inp, dims[0], 1, 1, 0),
+            ConvBlock(dims[0], dims[0], 3, 1, 1),
+            ConvBlock(dims[0], dims[0], 3, 1, 1),
         )
 
         self.branch2 = nn.Sequential(
-            ConvBlock(in_channels, dims[1], 1, stride=1, padding=0, norm=norm),
-            ConvBlock(dims[1], dims[1], 3, stride=1, padding=1, norm=norm),
+            ConvBlock(inp, dims[1], 1, 1, 0),
+            ConvBlock(dims[1], dims[1], 3, 1, 1),
         )
 
         self.branch3 = nn.Sequential(
-            nn.AvgPool2d(kernel_size=3, stride=1, padding=1),
-            ConvBlock(in_channels, dims[2], 1, stride=1, padding=0, norm=norm),
+            nn.AvgPool2d(3, 1, 1),
+            ConvBlock(inp, dims[2], 1, 1, 0),
         )
-        self.branch4 = ConvBlock(
-            in_channels,
-            dims[3],
-            kernel_size=1,
-            stride=1,
-            padding=0,
-            norm=norm,
-        )
+        self.branch4 = ConvBlock(inp, dims[3], 1, 1, 0)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+    def forward(self, x: Tensor) -> Tensor:
         b1 = self.branch1(x)
         b2 = self.branch2(x)
         b3 = self.branch3(x)
@@ -79,109 +58,33 @@ class Inceptionx3(nn.Module):
 
 
 class Inceptionx5(nn.Module):
-
     def __init__(
         self,
-        in_channels: int,
+        inp: int,
         dims: List[int],
-        norm: bool,
         n: int = 7,
     ) -> None:
         super().__init__()
         self.branch1 = nn.Sequential(
-            ConvBlock(
-                in_channels,
-                dims[0],
-                1,
-                stride=1,
-                padding=0,
-                norm=norm,
-            ),
-            ConvBlock(
-                dims[0],
-                dims[0],
-                kernel_size=(1, n),
-                stride=1,
-                padding=(0, n // 2),
-                norm=norm,
-            ),
-            ConvBlock(
-                dims[0],
-                dims[0],
-                kernel_size=(n, 1),
-                stride=1,
-                padding=(n // 2, 0),
-                norm=norm,
-            ),
-            ConvBlock(
-                dims[0],
-                dims[0],
-                kernel_size=(1, n),
-                stride=1,
-                padding=(0, n // 2),
-                norm=norm,
-            ),
-            ConvBlock(
-                dims[0],
-                dims[0],
-                kernel_size=(n, 1),
-                stride=1,
-                padding=(n // 2, 0),
-                norm=norm,
-            ),
+            ConvBlock(inp, dims[0], 1, 1, 0),
+            ConvBlock(dims[0], dims[0], (1, n), 1, (0, n // 2)),
+            ConvBlock(dims[0], dims[0], (n, 1), 1, (n // 2, 0)),
+            ConvBlock(dims[0], dims[0], (1, n), 1, (0, n // 2)),
+            ConvBlock(dims[0], dims[0], (n, 1), 1, (n // 2, 0)),
         )
 
         self.branch2 = nn.Sequential(
-            ConvBlock(
-                in_channels,
-                dims[1],
-                kernel_size=1,
-                stride=1,
-                padding=0,
-                norm=norm,
-            ),
-            ConvBlock(
-                dims[1],
-                dims[1],
-                kernel_size=(1, n),
-                stride=1,
-                padding=(0, n // 2),
-                norm=norm,
-            ),
-            ConvBlock(
-                dims[1],
-                dims[1],
-                kernel_size=(n, 1),
-                stride=1,
-                padding=(n // 2, 0),
-                norm=norm,
-            ),
+            ConvBlock(inp, dims[1], 1, 1, 0),
+            ConvBlock(dims[1], dims[1], (1, n), 1, (0, n // 2)),
+            ConvBlock(dims[1], dims[1], (n, 1), 1, (n // 2, 0)),
         )
 
         self.branch3 = nn.Sequential(
-            nn.AvgPool2d(
-                kernel_size=(3, 3),
-                stride=(1, 1),
-                padding=1,
-            ),
-            ConvBlock(
-                in_channels,
-                dims[2],
-                kernel_size=1,
-                stride=1,
-                padding=0,
-                norm=norm,
-            ),
+            nn.AvgPool2d(3, 1, 1),
+            ConvBlock(inp, dims[2], 1, 1, 0),
         )
 
-        self.branch4 = ConvBlock(
-            in_channels,
-            dims[3],
-            kernel_size=1,
-            stride=1,
-            padding=0,
-            norm=norm,
-        )
+        self.branch4 = ConvBlock(inp, dims[3], 1, 1, 0)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         b1 = self.branch1(x)
@@ -192,99 +95,33 @@ class Inceptionx5(nn.Module):
 
 
 class Inceptionx2(nn.Module):
-
     def __init__(
         self,
         in_channels: int,
         dims: List[int],
-        norm: bool,
     ) -> None:
         super().__init__()
         self.branch1 = nn.Sequential(
-            ConvBlock(
-                in_channels,
-                dims[0] // 4,
-                kernel_size=1,
-                stride=1,
-                padding=0,
-                norm=norm,
-            ),
-            ConvBlock(
-                dims[0] // 4,
-                dims[0] // 4,
-                kernel_size=3,
-                stride=1,
-                padding=1,
-                norm=norm,
-            ),
+            ConvBlock(in_channels, dims[0] // 4, 1, 1, 0),
+            ConvBlock(dims[0] // 4, dims[0] // 4, 3, 1, 1),
         )
 
-        self.branch1_1 = ConvBlock(
-            dims[0] // 4,
-            dims[0],
-            kernel_size=(1, 3),
-            stride=1,
-            padding=(0, 3 // 2),
-            norm=norm,
-        )
+        self.branch1_1 = ConvBlock(dims[0] // 4, dims[0], (1, 3), 1, (0, 3 // 2))
+        self.branch1_2 = ConvBlock(dims[0] // 4, dims[1], (3, 1), 1, (3 // 2, 0))
+        self.branch2 = ConvBlock(in_channels, dims[2] // 4, 1, 1, 0)
 
-        self.branch1_2 = ConvBlock(
-            dims[0] // 4,
-            dims[1],
-            kernel_size=(3, 1),
-            stride=1,
-            padding=(3 // 2, 0),
-            norm=norm,
-        )
+        self.branch2_1 = ConvBlock(dims[2] // 4, dims[2], (1, 3), 1, (0, 3 // 2))
 
-        self.branch2 = ConvBlock(
-            in_channels,
-            dims[2] // 4,
-            kernel_size=1,
-            stride=1,
-            padding=0,
-            norm=norm,
-        )
-
-        self.branch2_1 = ConvBlock(
-            dims[2] // 4,
-            dims[2],
-            kernel_size=(1, 3),
-            stride=1,
-            padding=(0, 3 // 2),
-            norm=norm,
-        )
-
-        self.branch2_2 = ConvBlock(
-            dims[2] // 4,
-            dims[3],
-            kernel_size=(3, 1),
-            stride=1,
-            padding=(3 // 2, 0),
-            norm=norm,
-        )
+        self.branch2_2 = ConvBlock(dims[2] // 4, dims[3], (3, 1), 1, (3 // 2, 0))
 
         self.branch3 = nn.Sequential(
             nn.MaxPool2d(kernel_size=3, stride=1, padding=1),
-            ConvBlock(
-                in_channels,
-                dims[4],
-                kernel_size=1,
-                stride=1,
-                padding=0,
-                norm=norm,
-            ))
-
-        self.branch4 = ConvBlock(
-            in_channels,
-            dims[5],
-            kernel_size=1,
-            stride=1,
-            padding=0,
-            norm=norm,
+            ConvBlock(in_channels, dims[4], 1, 1, 0),
         )
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        self.branch4 = ConvBlock(in_channels, dims[5], 1, 1, 0)
+
+    def forward(self, x: Tensor) -> Tensor:
         b1 = self.branch1(x)
         b1_1 = self.branch1_1(b1)
         b1_2 = self.branch1_2(b1)
@@ -294,77 +131,41 @@ class Inceptionx2(nn.Module):
         b3 = self.branch3(x)
         b4 = self.branch4(x)
         return torch.cat(
-            [
-                b1_1,
-                b1_2,
-                b2_1,
-                b2_2,
-                b3,
-                b4,
-            ],
+            [b1_1, b1_2, b2_1, b2_2, b3, b4],
             dim=1,
         )
 
 
 class GridReduction(nn.Module):
-
     def __init__(
         self,
-        in_channels: int,
-        out_channels: int,
-        norm: bool,
+        inp: int,
+        outp: int,
     ) -> None:
         super().__init__()
 
-        self.conv = ConvBlock(
-            in_channels=in_channels,
-            out_channels=out_channels,
-            kernel_size=3,
-            stride=2,
-            padding=0,
-            norm=norm,
-        )
+        self.conv = ConvBlock(inp, outp, 3, 2, 0)
+        self.pooling = nn.MaxPool2d(3, 2)
 
-        self.pooling = nn.MaxPool2d(kernel_size=3, stride=2)
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+    def forward(self, x: Tensor) -> Tensor:
         conv = self.conv(x)
         pooling = self.pooling(x)
         return torch.cat([conv, pooling], dim=1)
 
 
 class AuxClassifier(nn.Module):
-
     def __init__(
         self,
-        in_channels: int,
-        num_classes: int,
-        norm: bool,
+        inp: int,
+        outp: int,
     ) -> None:
         super().__init__()
-        self.pooling = nn.AvgPool2d(
-            kernel_size=(5, 5),
-            stride=3,
-        )
-        self.conv_1 = ConvBlock(
-            in_channels=in_channels,
-            out_channels=128,
-            kernel_size=1,
-            stride=1,
-            padding=1,
-            norm=norm,
-        )
-        self.conv_2 = ConvBlock(
-            in_channels=128,
-            out_channels=768,
-            kernel_size=5,
-            stride=1,
-            padding=1,
-            norm=norm,
-        )
+        self.pooling = nn.AvgPool2d(5, 3)
+        self.conv_1 = ConvBlock(inp, 128, 1, 1, 1)
+        self.conv_2 = ConvBlock(128, 768, 5, 1, 1)
         self.flatten = nn.Flatten()
         self.avgpooling = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(768, num_classes)
+        self.fc = nn.Linear(768, outp)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.pooling(x)
--- models/InceptionNet/models.py
+++ models/InceptionNet/models.py
@@ -1,100 +1,49 @@
+from typing import Union, Tuple
 from turtle import forward
 import torch
 from torch import nn
+from torch import Tensor
 
-from .blocks import AuxClassifier, Inceptionx2, Inceptionx3, Inceptionx5, ConvBlock, GridReduction
+from .blocks import *
 
-from typing import Union, Tuple
 
 MODEL_RETURN_TYPE = Union[Tuple[torch.Tensor, torch.Tensor], torch.Tensor]
 
 
 class Inception_v3(nn.Module):
-
     def __init__(
         self,
         image_channels: int,
         num_classes: int,
-        norm: bool,
     ) -> None:
         super().__init__()
 
-        self.conv1 = ConvBlock(
-            in_channels=image_channels,
-            out_channels=32,
-            kernel_size=3,
-            stride=2,
-            padding=0,
-            norm=norm,
-        )
-
-        self.conv2 = ConvBlock(
-            in_channels=32,
-            out_channels=32,
-            kernel_size=3,
-            stride=1,
-            padding=0,
-            norm=norm,
-        )
-
-        self.conv3 = ConvBlock(
-            in_channels=32,
-            out_channels=64,
-            kernel_size=3,
-            stride=1,
-            padding=1,
-            norm=norm,
-        )
-
-        self.pool = nn.MaxPool2d(kernel_size=3, stride=2)
-
-        self.conv4 = ConvBlock(
-            in_channels=64,
-            out_channels=80,
-            kernel_size=3,
-            stride=1,
-            padding=0,
-            norm=norm,
-        )
-
-        self.conv5 = ConvBlock(
-            in_channels=80,
-            out_channels=192,
-            kernel_size=3,
-            stride=2,
-            padding=0,
-            norm=norm,
-        )
-
-        self.conv6 = ConvBlock(
-            in_channels=192,
-            out_channels=288,
-            kernel_size=3,
-            stride=1,
-            padding=1,
-            norm=norm,
-        )
+        self.conv1 = ConvBlock(image_channels, 32, 3, 2, 0)
+        self.conv2 = ConvBlock(32, 32, 3, 1, 0)
+        self.conv3 = ConvBlock(32, 64, 3, 1, 1)
+        self.pool = nn.MaxPool2d(3, 2)
+        self.conv4 = ConvBlock(64, 80, 3, 1, 0)
+        self.conv5 = ConvBlock(80, 192, 3, 2, 0)
+        self.conv6 = ConvBlock(192, 288, 3, 1, 1)
 
         self.inceptionx3 = nn.Sequential(
-            Inceptionx3(in_channels=288, dims=[96, 96, 96, 96], norm=norm),
-            Inceptionx3(in_channels=96 * 4, dims=[96, 96, 96, 96], norm=norm),
-            Inceptionx3(in_channels=96 * 4, dims=[96, 96, 96, 96], norm=norm),
-            GridReduction(in_channels=96 * 4, out_channels=96 * 4, norm=norm),
+            Inceptionx3(inp=288, dims=[96] * 4),
+            Inceptionx3(inp=96 * 4, dims=[96] * 4),
+            Inceptionx3(inp=96 * 4, dims=[96] * 4),
+            GridReduction(inp=96 * 4, outp=96 * 4),
         )
         self.aux = AuxClassifier(
-            in_channels=96 * 4 * 2,
-            num_classes=num_classes,
-            norm=norm,
+            inp=96 * 4 * 2,
+            outp=num_classes,
         )
 
         self.inceptionx5 = nn.Sequential(
-            Inceptionx5(in_channels=96 * 4 * 2, dims=[160] * 4, norm=norm),
-            Inceptionx5(in_channels=160 * 4, dims=[160] * 4, norm=norm),
-            Inceptionx5(in_channels=160 * 4, dims=[160] * 4, norm=norm),
-            Inceptionx5(in_channels=160 * 4, dims=[160] * 4, norm=norm),
-            Inceptionx5(in_channels=160 * 4, dims=[160] * 4, norm=norm),
-            GridReduction(in_channels=160 * 4, out_channels=160 * 4,
-                          norm=norm),
+            Inceptionx5(inp=96 * 4 * 2, dims=[160] * 4),
+            Inceptionx5(inp=160 * 4, dims=[160] * 4),
+            Inceptionx5(inp=160 * 4, dims=[160] * 4),
+            Inceptionx5(inp=160 * 4, dims=[160] * 4),
+            Inceptionx5(inp=160 * 4, dims=[160] * 4),
+            GridReduction(inp=160 * 4, outp=160 * 4),
         )
 
         inceptionx2_dims = [
@@ -105,22 +54,20 @@ class Inception_v3(nn.Module):
             Inceptionx2(
                 in_channels=160 * 4 * 2,
                 dims=inceptionx2_dims[0],
-                norm=norm,
             ),
             Inceptionx2(
                 in_channels=sum(inceptionx2_dims[0]),
                 dims=inceptionx2_dims[1],
-                norm=norm,
             ),
         )
 
-        self.avgpool = nn.AdaptiveMaxPool2d(output_size=(1, 1))
+        self.avgpool = nn.AdaptiveMaxPool2d(1)
         self.classifier = nn.Sequential(
             nn.Flatten(),
             nn.Linear(sum(inceptionx2_dims[1]), num_classes),
         )
 
-    def forward(self, x: torch.Tensor) -> MODEL_RETURN_TYPE:
+    def forward(self, x: Tensor) -> MODEL_RETURN_TYPE:
         x = self.conv1(x)
         x = self.conv2(x)
         x = self.conv3(x)
@@ -136,22 +83,5 @@ class Inception_v3(nn.Module):
         x = self.avgpool(x)
         x = self.classifier(x)
 
-        if self.training:
-            return [x, aux]
-
-        return x
-
-    def initialize_weights(self):
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(
-                    m.weight,
-                    mode="fan_out",
-                    nonlinearity="relu",
-                )
-            elif isinstance(m, nn.BatchNorm2d):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
-            elif isinstance(m, nn.Linear):
-                nn.init.normal_(m.weight, 0, 0.01)
-                nn.init.constant_(m.bias, 0)
+        # AUX
+        return [x, aux] if self.training else x
