4fb40e8055d4b66830e739a635fc6a5cf876c77f
yslee <yslee.dev@gmail.com>
2022-03-04 00:18:25 +0900

    test: InceptionNet inference test

---
+++ tests/InceptionNet/conftest.py
@@ -0,0 +1,41 @@
+import pytest
+from models.InceptionNet.models import Inception_v3
+
+from easydict import EasyDict
+import torch
+
+
+@pytest.fixture(
+    scope="module",
+)
+def config():
+    return EasyDict(
+        {
+            "image_channels": 3,
+            "num_classes": 10,
+            "image_size": 224,
+        }
+    )
+
+
+@pytest.fixture(
+    scope="module",
+)
+def batch(config):
+    c = config.image_channels
+    w = h = config.image_size
+    return (
+        torch.rand(1, c, w, h),
+        torch.rand(1),
+    )
+
+
+@pytest.fixture(
+    scope="module",
+    params=[Inception_v3],
+)
+def model(request, config):
+    return request.param(
+        config.image_channels,
+        config.num_classes,
+    )
