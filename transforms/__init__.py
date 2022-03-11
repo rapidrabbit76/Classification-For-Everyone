28b76bc4f61ff94232625a91172d85580b7c1682
swlee <zkdlfrlwl3@naver.com>
2022-03-11 14:52:38 +0900

    feat: add transforms table

--- transforms/__init__.py
+++ transforms/__init__.py
@@ -1,11 +1,13 @@
 from .base import *
-
+from .medium import *
 
 TRANSFORMS_TABLE: Dict["str", Callable] = {
     "BASE": BaseTransforms,
+    "MEDUIM": MediumTransforms,
 }
 
 __all__ = [
     "BaseTransforms",
+    "MediumTransforms",
     "TRANSFORMS_TABLE",
 ]
--- transforms/medium.py
+++ transforms/medium.py
@@ -7,7 +7,7 @@ from PIL import Image
 import cv2
 
 
-class BaseTransforms:
+class MediumTransforms:
     mean = (0.485, 0.456, 0.406)
     std = (0.229, 0.224, 0.225)
 
